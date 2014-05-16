1da5201379e8e1ab7c63f8d7f7e0d05b23903f28
raphaelframos <raphaelframos@gmail.com>
2014-05-15 21:03:43 -0300

    Questao 2

--- src/2.cpp
+++ src/2.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+long double fibonacci(){
 
-	return 0;
+	int numeroAnterior = 1;
+	int numeroSeguinte = 2;
+	int numeroAuxiliar = 0;
+	long double sum = 1;
+	while(numeroSeguinte < 4000000){
+		if(numeroSeguinte%2 == 0){
+			sum += numeroSeguinte;
+		}
+		numeroAuxiliar = numeroAnterior;
+		numeroAnterior = numeroSeguinte;
+		numeroSeguinte = numeroAuxiliar + numeroAnterior;
+
+	}
+	return sum;
 }
+
+/*int main(){
+	cout.precision(7);
+	cout << "Resultado = " << fibonacci();
+	return 0;
+}*/
